b7ca6f6d8556c1e14fad547ecffeb60fadf7fe10
MarvinEngineer <dfghafhathjstjnt@gmail.com>
2016-09-22 13:41:47 +0300

    Утренний #6
    Откорректирована работа главного меню.
    Добавлено управление с клавиатуры. Кнопка выход работает для эдитора.

--- Assets/Menu/Scripts/MenuSwitcher.cs
+++ Assets/Menu/Scripts/MenuSwitcher.cs
@@ -1,25 +1,35 @@
 ﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
-
-
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using System;
 
 public class MenuSwitcher : MonoBehaviour {
 
-    private LevelLoader loader = new LevelLoader();
+    private float timer = 1f;
 
-    public void SelectMainMenu (string _c)
+    private List<MItem> list = new List<MItem>();
+
+    private MItem currentSelected;
+
+    public event EventHandler<ButtonEventArgs> SelectedChange = delegate { };
+
+    private void ReleaseMenuItem (object sender, ButtonEventArgs e)
     {
-        switch(_c)
+        switch(e.Name)
         {
             case "exit":
                 {
+#if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+#else
                     Application.Quit();
+#endif
+
                     break;
                 }
             case "new":
                 {
-                    loader.LoadALevel("newScene");
+                    gameObject.GetComponent<LevelLoader>().LoadALevel("newScene");
                     break;
                 }
             case "load":
@@ -30,7 +40,99 @@ public class MenuSwitcher : MonoBehaviour {
                 {
                     break;
                 }
-        }       
-    }    
+        }  
+             
+    }
+
+    private void SelectMenuItem(object sender, ButtonEventArgs e)
+    {
+        foreach (MItem item in list)
+            if (item.name == e.Name)
+            {
+                item.isActive = true;
+                currentSelected = item;
+            }
+    }
+
+    private void DeselectedItem(object sender, ButtonEventArgs e)
+    {
+        foreach (MItem item in list)
+            if (item.name == e.Name) item.isActive = false;
+        currentSelected = null;
+    }
+
+    private void ChangeSelect(float input)
+    {
+        if (currentSelected != null)
+        {
+            list[currentSelected.index - 1].isActive = false;
+            currentSelected.isActive = false;
+            if (input < 0)
+            {
+                if (currentSelected.index != list.Count) currentSelected = list[currentSelected.index];
+                else return;
+            }
+
+            if (input > 0)
+            {
+                if (currentSelected.index != 1) currentSelected = list[currentSelected.index-2];
+                else return;
+            }
+        }
+        else currentSelected = list[0];
+
+        list[currentSelected.index-1].isActive = true;
+        SelectedChange(this, new ButtonEventArgs(currentSelected.name));
+    }
+
+    void Start()
+    {       
+        gameObject.AddComponent<LevelLoader>();
+        Menu_item_control[] array = GetComponentsInChildren<Menu_item_control>();
+
+        int i = 1;
+        foreach (Menu_item_control c in array)
+        {
+            c.released += ReleaseMenuItem;
+            c.selected += SelectMenuItem;
+            c.deselected += DeselectedItem;
+            SelectedChange += c.UpdateSelect;
+
+            list.Add(new MItem(i, c.name, false));
+            i++;
+        }
+    }
+
+    void Update()
+    {
+        if (timer<1f)
+            timer+=Time.deltaTime;
+
+        if ((Input.GetAxisRaw("Vertical") != 0)&&(timer>=0.25f))
+        {
+            ChangeSelect(Input.GetAxisRaw("Vertical"));
+            timer = 0;
+        }
+    }
 }
+
+public class MItem
+{
+    public int index { get; set; }
+    public string name { get; set; }
+    public bool isActive { get; set; }
+
+    private MItem()
+    {
+
+    }
+
+    public MItem(int i, string s, bool a)
+    {
+        index = i;
+        name = s;
+        isActive = a;
+    }
+}
+
 
--- Assets/Menu/Scripts/Menu_item_control.cs
+++ Assets/Menu/Scripts/Menu_item_control.cs
@@ -1,40 +1,49 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System;
+using UnityEngine;
 
 public class Menu_item_control : MonoBehaviour {
 
-    protected TextMesh text;    
     private GameObject _cube;
     public GameObject cube;
-    public GameObject switcher;
+
+    private bool isSelected;
+
+    public event EventHandler<ButtonEventArgs> selected = delegate { };
+    public event EventHandler<ButtonEventArgs> deselected = delegate { };
+
+    public event EventHandler<ButtonEventArgs> released = delegate { };
 
     // Use this for initialization
     void Start () {
-        text = this.GetComponent<TextMesh>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
+        isSelected = false;
+    }
+
+    void Update()
+    {
+        if (isSelected)
+        {
+            RotateCube();
+            if (Input.GetButton("Submit"))
+                released(this, new ButtonEventArgs(name)); 
+        }
     }
 
     void OnMouseEnter()
     {
-        text.color = Color.red;
-        _cube = Instantiate(cube, this.transform.position - new Vector3(7, 5, 0), this.transform.rotation) as GameObject;
+        Select();
+        selected(this, new ButtonEventArgs(name));
     }
 
     void OnMouseOver()
     {
-        RotateCube();
         if (Input.GetButtonDown("Fire1"))
-            switcher.GetComponent<MenuSwitcher>().SelectMainMenu(this.name);
+            released(this, new ButtonEventArgs(name));
     }
 
     void OnMouseExit()
     {
-        text.color = Color.white;
-        Destroy(_cube);
+        Deselect();
+        deselected(this, new ButtonEventArgs(name));
     }
 
     private void RotateCube()
@@ -42,4 +51,30 @@ public class Menu_item_control : MonoBehaviour {
         if (_cube != null)
             _cube.transform.Rotate((new Vector3(1, 1, 0)), 100 * Time.deltaTime);
     }
+
+    private void Select()
+    {        
+        GetComponent<TextMesh>().color = Color.red;
+        _cube = Instantiate(cube, this.transform.position - new Vector3(7, 5, 0), this.transform.rotation) as GameObject;
+        isSelected = true;
+    }
+
+    public void UpdateSelect(object sender, ButtonEventArgs e)
+    {
+        if (e.Name == name) Select();
+        else if (isSelected) Deselect();
+    }
+
+    private void Deselect()
+    {
+        GetComponent<TextMesh>().color = Color.white;
+        if (_cube != null) Destroy(_cube);
+        isSelected = false;
+    }
+}
+
+public class ButtonEventArgs: EventArgs
+{
+    public string Name;
+    public ButtonEventArgs(string name) { Name = name; }
 }
